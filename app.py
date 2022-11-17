ffe7e0c1d165b64ec7116fbadf7084f9d75153fb
dpizarrow <diego.pizarro0@gmail.com>
2022-11-17 16:00:39 -0300

    Use bootstrap

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Price should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='El precio deberia ser $ {} para ser rentable'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
