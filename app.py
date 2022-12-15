e7bda35adb188319ea68b5901b23926c5b5fa419
dpizarrow <diego.pizarro0@gmail.com>
2022-12-15 16:10:39 -0300

    Actualizar modelo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-model = pickle.load(open('ridge.pkl', 'rb'))
+model = pickle.load(open('xgb_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,11 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
+
+    print(output)
 
     return render_template('index.html', prediction_text='El precio deberia ser $ {} para ser rentable'.format(output))
 
