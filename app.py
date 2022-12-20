4f2d8f495cce26e515ed7c71a7cd1f1e085ae07f
Carlos A. Vásquez <carlos.vasquez@ideauno.cl>
2022-12-19 21:26:23 -0300

    feat: Se agregan variables para mostrar en la salida

--- app.py
+++ app.py
@@ -18,11 +18,17 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0], 0)
 
     print(output)
 
-    return render_template('index.html', prediction_text='El precio deberia ser $ {} para ser rentable'.format(output))
+    # noches disponibles
+    nights = request.form.get("availability_30")
+
+    # monto ganado al mes
+    month = nights * output
+
+    return render_template('index.html', prediction_text=output, nights=nights, month=month)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
