2616f148d8ede20e3a734ff1d3bd27350c0aa533
Carlos A. Vásquez <carlos.vasquez@ideauno.cl>
2022-12-19 21:45:20 -0300

    Rollback

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def chilean_currency(amount):
     currency = "${:,.1f}".format(amount)
     main_currency, fractional_currency = currency.split('.')
     new_main_currency = main_currency.replace(',', '.')
-    return new_main_currency
+    currency = new_main_currency
 
 @app.route('/')
 def home():
@@ -24,7 +24,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = chilean_currency(prediction[0])
+    output = round(prediction[0], 0)
 
     print(output)
 
@@ -32,7 +32,7 @@ def predict():
     nights = int(request.form.get("availability_30"))
 
     # monto ganado al mes
-    month = chilean_currency(nights * int(output))
+    month = nights * int(output)
 
     return render_template('index.html', prediction_text=output, nights=nights, month=month)
 
